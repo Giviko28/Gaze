20855be289b381933b40d6e009492d59c59a7924
Giviko <chelidze.givia@gmail.com>
2023-09-03 20:05:34 +0400

    Breeze + Navbar setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,28 @@
-<h1></h1>
-<pre>
-    {!! $html !!}
-</pre>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gaze</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="min-h-screen pt-8 px-16">
+    <nav>
+        <div class="flex items-center whitespace-nowrap">
+            <img class="h-16 w-16" src="{{ asset('images/gaze.png') }}" alt="Logo">
+            <div class="border-l-2 border-slate-300 my-4 mx-4">&nbsp;</div>
+
+            <div class="space-x-2">
+                <a href="">Dashboard</a>
+                <a href="">Services</a>
+                <a href="">About</a>
+                <a href="">Contact Us</a>
+            </div>
+
+        </div>
+
+    </nav>
+</body>
+</html>
