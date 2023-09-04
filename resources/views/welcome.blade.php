1baf9fd840b5e6e1f60e090ea485971ccdaa8efa
Giviko <chelidze.givia@gmail.com>
2023-09-04 23:50:52 +0400

    More landing page stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
                 <div class="hidden md:block border-l-2 border-first my-4 mx-4">&nbsp;</div>
 
                 <div class="hidden space-x-2 text-fourth md:block ">
-                    <a href="">Dashboard</a>
+                    <a href="/dashboard">Dashboard</a>
                     <a href="">Services</a>
                     <a href="">About</a>
                     <a href="">Contact Us</a>
@@ -45,8 +45,9 @@
         <script src="{{ asset('js/navbar.js') }}"></script>
     </div>
 
-    <main class="lg:px-20 px-6">
-        <div class="flex-col md:flex-row md:flex py-12 space-y-6 md:space-y-0">
+    <main>
+        <!-- Display --->
+        <div class="lg:px-20 px-6 flex-col md:flex-row md:flex py-12 space-y-6 md:space-y-0 bg-gradient-to-b from-white via-gray-50 to-purple-50">
             <!-- Left side -->
             <div class="flex flex-col items-center w-full md:w-1/2 justify-center">
                 <div class="space-y-8">
@@ -62,7 +63,25 @@
             </div>
             <!-- Right side -->
             <div class="md:w-1/2">
-                <img class="object-cover w-full h-96" src="{{ asset('images/landing2.png') }}" alt="Cool image">
+                <img class="object-cover w-full h-96" src="{{ asset('images/landing3.png') }}" alt="Cool image">
+            </div>
+        </div>
+        <!-- Extra Info -->
+        <div class="grid grid-cols-1 md:grid-cols-3 md:gap-4 gap-8 px-8 py-6">
+            <div class="p-4 space-y-1.5 flex flex-col justify-center items-center">
+                <p class="text-6xl md:text-4xl text-fourth font-semibold">1 million</p>
+                <p class="text-xl md:text-base text-fourth font-light">Product on watchlist</p>
+            </div>
+            <div class="p-4 space-y-1.5 flex flex-col justify-center items-center">
+                <div class="space-x-2 flex items-center">
+                    <p class="text-6xl md:text-4xl text-fourth font-semibold">50+</p>
+                    <span class="bg-third p-1.5 rounded-xl text-white">counting...</span>
+                </div>
+                <p class="text-xl md:text-base text-fourth font-light">Websites supported</p>
+            </div>
+            <div class="p-4 space-y-1.5 flex flex-col justify-center items-center">
+                <p class="text-6xl md:text-4xl text-fourth font-semibold">1 platform</p>
+                <p class="text-xl md:text-base text-fourth font-light">Instead of all</p>
             </div>
         </div>
     </main>
